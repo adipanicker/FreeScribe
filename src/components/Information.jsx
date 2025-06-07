835ddb6176b9d777eb0dea0ac05ef48a7bc11c59
Aditya Panicker <adipanicker7@gmail.com>
2025-06-07 18:35:43 +0530

    updates

--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -1,9 +1,81 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Transcription from "./Transcription";
 import Translation from "./Translation";
 
-export default function Information() {
+export default function Information(props) {
+  const { output } = props;
+  console.log(output);
   const [tab, setTab] = useState("transcription");
+  const [translation, setTranslation] = useState(null);
+  const [toLanguage, setToLanguage] = useState("Select Language");
+  const [translating, setTranslating] = useState(null);
+
+  const worker = useRef();
+  const textElement =
+    tab === "transcription"
+      ? output.map((val) => val.text)
+      : translation || "No translation";
+
+  useEffect(() => {
+    if (!worker.current) {
+      worker.current = new Worker(
+        new URL("../utils/translate.worker.js", import.meta.url),
+        {
+          type: "module",
+        }
+      );
+    }
+
+    const onMessageReceived = async (e) => {
+      switch (e.data.status) {
+        case "initiate":
+          console.log("DOWNLOADING");
+          break;
+        case "progress":
+          console.log("LOADING");
+          break;
+        case "update":
+          setTranslation(e.data.output);
+          console.log(e.data.output);
+          break;
+        case "complete":
+          setTranslating(false);
+          console.log("DONE");
+          break;
+      }
+    };
+
+    worker.current.addEventListener("message", onMessageReceived);
+
+    return () =>
+      worker.current.removeEventListener("message", onMessageReceived);
+  });
+
+  function generateTranslation() {
+    if (translating || toLanguage === "Select language") {
+      return;
+    }
+    setTranslating(true);
+
+    worker.current.postMessage({
+      text: output.map((val) => val.text),
+      src_language: "eng_Latn",
+      tgt_lang: toLanguage,
+    });
+  }
+  function handleCopies() {
+    navigator.clipboard.writeText(textElement);
+  }
+
+  function handleDownload() {
+    const element = document.createElement("a");
+    const file = new Blob([textElement], { type: "text/plain" });
+    element.href = URL.createObjectURL(file);
+    element.download = `Freescribe_${new Date().toString()}.txt`;
+    document.body.appendChild(element);
+    element.click();
+  }
+
   return (
     <main
       className="flex-1 p-4 flex flex-col gap-3 
@@ -16,7 +88,7 @@ export default function Information() {
       <div className="grid grid-cols-2 mx-auto bg-white border-2 border-solid border-blue-400 shadow rounded-full overflow-hidden items-center font-poppins">
         <button
           onClick={() => setTab("transcription")}
-          className={`px-4 py-1 duration-200 font-medium ${
+          className={`px-4 py-1 duration-200  ${
             tab === "transcription"
               ? "bg-blue-400 text-white"
               : "text-blue-400 hover:text-blue-600"
@@ -26,7 +98,7 @@ export default function Information() {
         </button>
         <button
           onClick={() => setTab("translation")}
-          className={`px-4 py-1 duration-200 font-medium ${
+          className={`px-4 py-1 duration-200  ${
             tab === "translation"
               ? "bg-blue-400 text-white"
               : "text-blue-400 hover:text-blue-600"
@@ -35,7 +107,38 @@ export default function Information() {
           Translation
         </button>
       </div>
-      {tab === "transcription" ? <Transcription /> : <Translation />}
+      <div className="my-8 flex flex-col">
+        {tab === "transcription" ? (
+          <Transcription {...props} textElement={textElement} />
+        ) : (
+          <Translation
+            {...props}
+            toLanguage={toLanguage}
+            translating={translating}
+            textElement={textElement}
+            setTranslation={setTranslation}
+            setTranslating={setTranslating}
+            setToLanguage={setToLanguage}
+            generateTranslation={generateTranslation}
+          />
+        )}
+      </div>
+      <div className="flex items-center gap-4 mx-auto text-base">
+        <button
+          title="Copy"
+          onClick={handleCopies}
+          className="bg-white hover:text-blue-500 duration-200 text-blue-300 p-2 rounded px-4"
+        >
+          <i className="fa-solid fa-copy "></i>
+        </button>
+        <button
+          title="Download"
+          onClick={handleDownload}
+          className="bg-white hover:text-blue-500 duration-200 text-blue-300 p-2 rounded px-4"
+        >
+          <i className="fa-solid fa-download "></i>
+        </button>
+      </div>
     </main>
   );
 }
--- src/components/Transcription.jsx
+++ src/components/Transcription.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 export default function Transcription(props) {
   const { textElement } = props;
+  console.log("textElement:", textElement, "type:", typeof textElement);
 
   return <div>{textElement}</div>;
 }
